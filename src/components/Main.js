8e19ff6f8784a45d80f411394d425ec06c312af6
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-09 02:44:57 -0800

    imported components and made form for username input and added style

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,42 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Card, CardSection, Button, Input } from '../common';
 
 class Main extends Component {
+  state = {
+    username: ''
+  }
+
   render() {
     return(
-      <View>
-        <Text>
-          Main Screen
-        </Text>
-      </View>
+      <Card>
+        <Text style={styles.headerText}>Chit Chat</Text>
+
+        <CardSection>
+          <Input
+            placeholder='Username'
+            label='Username'
+            value={this.state.username}
+            onChangeText={username => this.setState({ username })}
+          />
+        </CardSection>
+
+        <CardSection>
+          <Button
+            onPress={() => this.props.navigation.navigate('Chat')}
+          >
+            Enter
+          </Button>
+        </CardSection>
+      </Card>
     )
+  }
+}
+
+const styles = {
+  headerText: {
+    fontSize: 25,
+    textAlign: 'center'
   }
 }
 
