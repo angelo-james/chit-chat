ae75803350ea3f85f9db8d24664d6111f9154a66
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-09 03:11:42 -0800

    installed react-native-gifted-chat

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 class Chat extends Component {
+  static navigationOptions = ({ navigation }) => ({
+    title: (navigation.state.params || {}).name || 'Chat!'
+  })
+
+  state = {
+    messages: []
+  }
+
   render() {
     const { username } = this.props.navigation.state.params;
 
     return (
-      <View>
-        <Text>
-          { username }
-        </Text>
-      </View>
+      <GiftedChat 
+        messages={this.state.messages} 
+      />
     )
   }
 }
