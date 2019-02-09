95cecaf9082c0214841906a7bfaa673a8fde8eac
Angelo James Arriola <angelojames.arriola@gmail.com>
2019-02-09 01:44:24 -0800

    installed react-navigation and create AppNavigator file

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,2 +1,11 @@
 import Main from './src/components/Main';
 import Chat from './src/components/Chat';
+
+import { createStackNavigator } from 'react-navigation';
+
+const navigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat }
+})
+
+export default navigator;
